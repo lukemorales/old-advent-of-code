bd35c945a254d2bc1d4a30f2afee9538ec79bc48
Luke Morales <lukemorales@live.com>
2022-12-02 01:36:41 -0300

    2022: complete day 01 challenge

--- 2022/day-01/functional.ts
+++ 2022/day-01/functional.ts
@@ -1,6 +1,5 @@
 import {
   option as O,
-  either as E,
   string as S,
   number as N,
   readonlyArray as A,
@@ -9,6 +8,115 @@ import {
   function as F,
 } from "fp-ts";
 
-export const functionalProgramA = (input: string) => null;
+export const functionalProgramA = (input: string) => {
+  const values = F.pipe(input, S.split("\n"));
 
-export const functionalProgramB = (input: string) => null;
+  const lookup = {
+    currentCalories: 0,
+    highestCalories: 0,
+  };
+
+  for (const index in values) {
+    const currentCalories = F.pipe(
+      values,
+      A.lookup(parseInt(index)),
+      O.map(F.flow(S.trim, parseInt)),
+      O.flatMap(O.fromPredicate(N.isNumber)),
+      O.map(N.MonoidSum.combine(lookup.currentCalories)),
+      O.toUndefined
+    );
+
+    if (!currentCalories) {
+      lookup.highestCalories = F.pipe(
+        lookup.highestCalories >= lookup.currentCalories,
+        B.match(
+          () => lookup.currentCalories,
+          () => lookup.highestCalories
+        )
+      );
+
+      lookup.currentCalories = 0;
+      continue;
+    }
+
+    if (A.isOutOfBound(parseInt(index), values)) {
+      lookup.highestCalories = F.pipe(
+        lookup.highestCalories >= currentCalories,
+        B.match(
+          () => currentCalories,
+          () => lookup.highestCalories
+        )
+      );
+    }
+
+    lookup.currentCalories = currentCalories;
+  }
+
+  return lookup.highestCalories;
+};
+
+export const functionalProgramB = (input: string) => {
+  const values = F.pipe(input, S.split("\n"));
+
+  const lookup = {
+    currentCalories: 0,
+    topRankingCalories: [0, 0, 0] as readonly [number, number, number],
+  };
+
+  const updateCaloriesRanking = (candidate: number) =>
+    F.pipe(
+      lookup.topRankingCalories,
+      A.head,
+      O.flatMap((topRanked) =>
+        F.pipe(
+          candidate,
+          O.fromPredicate((value) => value > topRanked)
+        )
+      ),
+      O.flatMap((value) =>
+        F.pipe(
+          lookup.topRankingCalories,
+          NEA.modifyAt(0, () => value),
+          O.map(
+            F.flow(
+              A.sort(N.Ord),
+              F.unsafeCoerce<
+                readonly number[],
+                typeof lookup["topRankingCalories"]
+              >
+            )
+          )
+        )
+      ),
+      O.getOrElse(() => lookup.topRankingCalories)
+    );
+
+  for (const index in values) {
+    const currentCalories = F.pipe(
+      values,
+      A.lookup(parseInt(index)),
+      O.map(F.flow(S.trim, parseInt)),
+      O.flatMap(O.fromPredicate(N.isNumber)),
+      O.map(N.MonoidSum.combine(lookup.currentCalories)),
+      O.toUndefined
+    );
+
+    if (!currentCalories) {
+      lookup.topRankingCalories = updateCaloriesRanking(lookup.currentCalories);
+      lookup.currentCalories = 0;
+
+      continue;
+    }
+
+    if (A.isOutOfBound(parseInt(index), values)) {
+      lookup.topRankingCalories = updateCaloriesRanking(currentCalories);
+    }
+
+    lookup.currentCalories = currentCalories;
+  }
+
+  return F.pipe(
+    lookup.topRankingCalories,
+    A.reduce(0, (total, current) => (total += current))
+  );
+};
--- 2022/day-01/standard.ts
+++ 2022/day-01/standard.ts
@@ -1,3 +1,87 @@
-export const programA = (input: string) => null;
+import assert from "assert";
 
-export const programB = (input: string) => null;
+export const programA = (input: string) => {
+  const values = input.split("\n");
+
+  const lookup = {
+    currentCalories: 0,
+    highestCalories: 0,
+  };
+
+  for (const index in values) {
+    const value = values[index]?.trim();
+    let currentCalories = lookup.currentCalories;
+
+    if (value === "") {
+      lookup.highestCalories =
+        lookup.highestCalories >= currentCalories
+          ? lookup.highestCalories
+          : currentCalories;
+
+      lookup.currentCalories = 0;
+      continue;
+    }
+
+    assert.ok(value);
+
+    const valueAsNumber = parseInt(value);
+    currentCalories += valueAsNumber;
+
+    if (parseInt(index) === values.length - 1) {
+      lookup.highestCalories =
+        lookup.highestCalories < currentCalories
+          ? currentCalories
+          : lookup.highestCalories;
+    }
+
+    lookup.currentCalories = currentCalories;
+  }
+
+  return lookup.highestCalories;
+};
+
+export const programB = (input: string) => {
+  const values = input.split("\n");
+
+  const lookup = {
+    currentCalories: 0,
+    topRankingCalories: [0, 0, 0] as [number, number, number],
+  };
+
+  const updateCaloriesRanking = (candidate: number): void => {
+    if (lookup.topRankingCalories[0] > candidate) {
+      return void null;
+    }
+
+    lookup.topRankingCalories[0] = candidate;
+    lookup.topRankingCalories.sort();
+  };
+
+  for (const index in values) {
+    const value = values[index]?.trim();
+    let currentCalories = lookup.currentCalories;
+
+    if (value === "") {
+      updateCaloriesRanking(currentCalories);
+
+      lookup.currentCalories = 0;
+      continue;
+    }
+
+    assert.ok(value);
+
+    const valueAsNumber = parseInt(value);
+    currentCalories += valueAsNumber;
+
+    if (parseInt(index) === values.length - 1) {
+      updateCaloriesRanking(currentCalories);
+    }
+
+    lookup.currentCalories = currentCalories;
+  }
+
+  return lookup.topRankingCalories.reduce(
+    (total, current) => (total += current),
+    0
+  );
+};
