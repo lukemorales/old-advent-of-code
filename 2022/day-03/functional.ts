a16a154bf567f67f6f1dabaac106b8b1e2a81f2b
Luke Morales <lukemorales@live.com>
2022-12-04 23:12:10 -0300

    2022: complete day 03 challenge

--- 2022/day-03/functional.ts
+++ 2022/day-03/functional.ts
@@ -1,14 +1,110 @@
 import {
   option as O,
-  either as E,
   string as S,
   number as N,
   readonlyArray as A,
-  readonlyNonEmptyArray as NEA,
-  boolean as B,
   function as F,
 } from "fp-ts";
 
-export const functionalProgramA = (input: string) => null;
+import { parseChallengeInput } from "../../utils/parse-challenge-input";
 
-export const functionalProgramB = (input: string) => null;
+const GROUP_LENGTH = 3;
+
+const getPriority = (char: string) =>
+  F.pipe(
+    "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".indexOf(char),
+    O.fromPredicate((index) => index > 0)
+  );
+
+export const functionalProgramA = (input: string) => {
+  return F.pipe(
+    input,
+    parseChallengeInput,
+    A.reduce(0, (sumOfPriorities, currentRucksack) => {
+      const halfOfRucksack = F.pipe(currentRucksack.length, N.Field.div(2));
+
+      const firstCompartmentItems = F.pipe(
+        currentRucksack,
+        S.slice(0, halfOfRucksack),
+        S.split("")
+      );
+
+      const secondCompartmentItems = F.pipe(
+        currentRucksack,
+        S.slice(halfOfRucksack, currentRucksack.length),
+        S.split("")
+      );
+
+      return F.pipe(
+        firstCompartmentItems,
+        A.findFirst((item) =>
+          F.pipe(secondCompartmentItems, F.pipe(item, A.elem(S.Eq)))
+        ),
+        O.flatMap(getPriority),
+        O.match(
+          () => sumOfPriorities,
+          (priorityValue) => (sumOfPriorities += priorityValue)
+        )
+      );
+    })
+  );
+};
+
+export const functionalProgramB = (input: string) => {
+  const rucksacks = F.pipe(input, parseChallengeInput);
+
+  let sumOfPriorities = 0;
+
+  for (let i = 0; i <= rucksacks.length; i += GROUP_LENGTH) {
+    type $RucksackItemsOption = O.Option<readonly [string, ...string[]]>;
+
+    const [first, second, third] = F.pipe(
+      [i, i + 1, i + 2],
+      A.map((index) => F.pipe(rucksacks, A.lookup(index), O.map(S.split("")))),
+      F.unsafeCoerce<
+        readonly $RucksackItemsOption[],
+        [$RucksackItemsOption, $RucksackItemsOption, $RucksackItemsOption]
+      >
+    );
+
+    const firstIntersection = F.pipe(
+      second,
+      O.map(
+        A.filter((x) =>
+          F.pipe(
+            first,
+            O.map((value) => new Set(value).has(x)),
+            O.getOrElse(() => false)
+          )
+        )
+      )
+    );
+
+    const secondIntersection = F.pipe(
+      third,
+      O.map(
+        A.filter((x) =>
+          F.pipe(
+            firstIntersection,
+            O.map((value) => new Set(value).has(x)),
+            O.getOrElse(() => false)
+          )
+        )
+      )
+    );
+
+    const item = F.pipe(
+      secondIntersection,
+      O.map((intersection) => [...new Set(intersection)]),
+      O.flatMap(A.head)
+    );
+
+    sumOfPriorities += F.pipe(
+      item,
+      O.flatMap(getPriority),
+      O.match(() => 0, F.identity)
+    );
+  }
+
+  return sumOfPriorities;
+};
--- 2022/day-03/standard.ts
+++ 2022/day-03/standard.ts
@@ -1,3 +1,71 @@
-export const programA = (input: string) => null;
+import { parseChallengeInput } from "../../utils/parse-challenge-input";
 
-export const programB = (input: string) => null;
+const GROUP_LENGTH = 3;
+const getPriority = (char: string) =>
+  "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".indexOf(char);
+
+export const programA = (input: string) => {
+  const rucksacks = parseChallengeInput(input);
+
+  return rucksacks.reduce((sumOfPriorities, currentRucksack) => {
+    const halfOfRucksack = Math.floor(currentRucksack.length / 2);
+
+    const firstCompartmentItems = currentRucksack
+      .slice(0, halfOfRucksack)
+      .split("");
+
+    const secondCompartmentItems = currentRucksack
+      .slice(halfOfRucksack, currentRucksack.length)
+      .split("");
+
+    const priorityItem = firstCompartmentItems.find((item) =>
+      secondCompartmentItems.includes(item)
+    );
+
+    if (!priorityItem) {
+      return sumOfPriorities;
+    }
+
+    const priorityValue = getPriority(priorityItem);
+
+    if (priorityValue === -1) {
+      throw new Error(`Invalid item: ${priorityItem}`);
+    }
+
+    return (sumOfPriorities += priorityValue);
+  }, 0);
+};
+
+export const programB = (input: string) => {
+  const rucksacks = parseChallengeInput(input);
+
+  let sumOfPriorities = 0;
+
+  for (let i = 0; i <= rucksacks.length; i += GROUP_LENGTH) {
+    const [first, second, third] = [
+      rucksacks[i],
+      rucksacks[i + 1],
+      rucksacks[i + 2],
+    ].map((sack) => sack?.split(""));
+
+    if (!first || !second || !third) {
+      continue;
+    }
+
+    const firstIntersection = second.filter((x) => new Set(first).has(x));
+
+    const secondIntersection = third.filter((x) =>
+      new Set(firstIntersection).has(x)
+    );
+
+    const [item] = [...new Set(secondIntersection)];
+
+    if (!item) {
+      continue;
+    }
+
+    sumOfPriorities += getPriority(item);
+  }
+
+  return sumOfPriorities;
+};
