37bb6bf5036eedf9186f368b6c2b1c69497209e5
Luke Morales <lukemorales@live.com>
2022-12-04 23:12:54 -0300

    2022: complete day 04 challenge

--- 2022/day-04/functional.ts
+++ 2022/day-04/functional.ts
@@ -1,14 +1,67 @@
 import {
-  option as O,
   either as E,
   string as S,
-  number as N,
   readonlyArray as A,
-  readonlyNonEmptyArray as NEA,
-  boolean as B,
   function as F,
 } from "fp-ts";
+import { parseChallengeInput } from "../../utils/parse-challenge-input";
 
-export const functionalProgramA = (input: string) => null;
+type Pair = `${number}-${number}`;
 
-export const functionalProgramB = (input: string) => null;
+export const functionalProgramA = (input: string) => {
+  return F.pipe(
+    input,
+    parseChallengeInput<`${Pair},${Pair}`>,
+    A.reduce(0, (totalOverlaps, currentPairs) => {
+      return F.pipe(
+        currentPairs,
+        S.split(","),
+        A.flatMap(
+          F.flow(
+            S.split("-"),
+            A.map((number) => parseInt(number))
+          )
+        ),
+        F.unsafeCoerce<
+          readonly number[],
+          readonly [number, number, number, number]
+        >,
+        E.fromPredicate(
+          ([leftStart, leftEnd, rightStart, rightEnd]) =>
+            (leftStart >= rightStart && leftEnd <= rightEnd) ||
+            (leftStart <= rightStart && leftEnd >= rightEnd),
+          () => 0
+        ),
+        E.match(
+          (_) => totalOverlaps,
+          (_) => (totalOverlaps += 1)
+        )
+      );
+    })
+  );
+};
+
+export const functionalProgramB = (input: string) => {
+  return F.pipe(
+    input,
+    parseChallengeInput<`${Pair},${Pair}`>,
+    A.reduce(0, (totalOverlaps, currentPairs) => {
+      return F.pipe(
+        currentPairs,
+        S.split(","),
+        A.flatMap(F.flow(S.split("-"), A.map(parseInt))),
+        F.unsafeCoerce<readonly number[], [number, number, number, number]>,
+        E.fromPredicate(
+          ([leftStart, leftEnd, rightStart, rightEnd]) =>
+            (rightStart <= leftEnd && rightStart >= leftStart) ||
+            (leftStart <= rightEnd && leftStart >= rightStart),
+          () => 0
+        ),
+        E.match(
+          (_) => totalOverlaps,
+          (_) => (totalOverlaps += 1)
+        )
+      );
+    })
+  );
+};
--- 2022/day-04/standard.ts
+++ 2022/day-04/standard.ts
@@ -1,3 +1,51 @@
-export const programA = (input: string) => null;
+import { parseChallengeInput } from "../../utils/parse-challenge-input";
 
-export const programB = (input: string) => null;
+type Pair = `${number}-${number}`;
+
+export const programA = (input: string) => {
+  const pairs = parseChallengeInput<`${Pair},${Pair}`>(input);
+
+  return pairs.reduce((totalOverlaps, currentPairs) => {
+    const [leftStart, leftEnd, rightStart, rightEnd] = currentPairs
+      .split(",")
+      .flatMap((pair) => pair.split("-").map((number) => parseInt(number))) as [
+      number,
+      number,
+      number,
+      number
+    ];
+
+    if (
+      (leftStart >= rightStart && leftEnd <= rightEnd) ||
+      (leftStart <= rightStart && leftEnd >= rightEnd)
+    ) {
+      return (totalOverlaps += 1);
+    }
+
+    return totalOverlaps;
+  }, 0);
+};
+
+export const programB = (input: string) => {
+  const pairs = parseChallengeInput<`${Pair},${Pair}`>(input);
+
+  return pairs.reduce((totalOverlaps, currentPairs) => {
+    const [leftStart, leftEnd, rightStart, rightEnd] = currentPairs
+      .split(",")
+      .flatMap((pair) => pair.split("-").map((number) => parseInt(number))) as [
+      number,
+      number,
+      number,
+      number
+    ];
+
+    if (
+      (rightStart <= leftEnd && rightStart >= leftStart) ||
+      (leftStart <= rightEnd && leftStart >= rightStart)
+    ) {
+      return (totalOverlaps += 1);
+    }
+
+    return totalOverlaps;
+  }, 0);
+};
