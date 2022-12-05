837b0b79570e193a9128da3bd6a094cd34bdc8bf
Luke Morales <lukemorales@live.com>
2022-12-05 00:33:54 -0300

    Add sample testing

--- 2022/day-04/index.ts
+++ 2022/day-04/index.ts
@@ -1,16 +1,21 @@
+import assert from "assert";
 import * as fs from "fs";
 import path from "path";
 import { functionalProgramA, functionalProgramB } from "./functional";
 import { programA, programB } from "./standard";
 
-const input = fs.readFileSync(path.join(__dirname, "_input.txt"), "utf-8");
+const INPUT = fs.readFileSync(path.join(__dirname, "_input.txt"), "utf-8");
+const SAMPLE = fs.readFileSync(path.join(__dirname, "_sample.txt"), "utf-8");
 
 const [a, b, fa, fb] = [
   programA,
   programB,
   functionalProgramA,
   functionalProgramB,
-].map((fn) => fn(input));
+].map((fn) => fn(INPUT));
+
+assert.deepStrictEqual(programA(SAMPLE), 2);
+assert.deepStrictEqual(programB(SAMPLE), 4);
 
 console.log(
   `${path.basename(__dirname)} => Part A: ${a} (${a === fa}) | Part B: ${b} (${
